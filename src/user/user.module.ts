bc3a1b968bd48ab07b30a033bf68b74341d0e622
895623741x <tommyhsu69@gmail.com>
2022-11-14 01:30:43 -0800

    create user controller

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { userSchema } from './schemas/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserController } from './user.controller';
 
 @Module({
-  providers: [UserService]
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: userSchema }])],
+
+  providers: [UserService],
+  controllers: [UserController],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './interfaces/user.interface';
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+}
